c3070622b0f0e34dba97e7add146e788fd2c02be
Ivan-Cecchi <i.cecchi@outlook.com>
2024-03-25 17:39:22 +0100

    EPICBlog v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivePostsComponent } from './active-posts/active-posts.component';
+import { InactivePostsComponent } from './inactive-posts/inactive-posts.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
